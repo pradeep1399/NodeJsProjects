589d518e2619254aa79a52dbad0e33f7c817b09d
pradeep1399 <pradeepverma1399@gmail.com>
2024-05-13 00:35:36 +0530

    adding files

---
+++ db.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+// Define the mongoDB connection URL
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/db'  // Replace 'db' with your database name
+
+// Set up MogoDB connection
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Get the default connection
+// Mongoose maintains a default connection object representing the MongoDB connection.
+const db = mongoose.connection;
+
+// Define event listeners for databse connection
+db.on('connected', () => {
+    console.log('Connected to MongoDB server');
+});
+
+db.on('error', (err) => {
+    console.error('MongoDb connection error:', err);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+// Export the database connection
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const db = require('./db');
+PORT = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/db'
+
+app.get('/HelloWorld', function (req, res) {
+    res.send('Hello World');
+});
+
+app.listen(PORT, (err) => {
+    if(err) console.log(err);
+    console.log('Server listening on Port', PORT);
+}) ;
+
