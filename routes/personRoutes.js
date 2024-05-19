860e73d1bc8f5a8073dc70dce082616ebaa2dbe7
pradeep1399 <pradeepverma1399@gmail.com>
2024-05-20 00:42:30 +0530

    update by id

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -44,4 +44,24 @@ router.get('/:workType', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+    try{
+        const personId = req.params.id; // Extract the id from the URL parameter
+        const updatePersonData = req.body; //Update data for the person
+        
+        const response = await Person.findByIdAndUpdate(personId, updatePersonData, {
+            new: true, //Return the updated document
+            runValidators: true // Run Mongoose validation
+        });
+        if(!response) {
+            return res.status(404).json({error: 'Person not found'});
+        }
+        console.log('data updated');
+        res.status(200).json(response);
+    } catch {
+        console.log(err);
+        res.status(500).json({ err: "interanl server error" });
+    }
+});
+
 module.exports = router;
